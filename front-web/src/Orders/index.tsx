fe1615c274dfbc59d0d10c76157d6c42846f8e62
Xande Raposo <jorgeanraposo@hotmail.com>
2021-01-10 09:54:56 -0300

    Add routes and StepsHeader

---
+++ front-web/src/Orders/index.tsx
@@ -0,0 +1,13 @@
+import { SSL_OP_DONT_INSERT_EMPTY_FRAGMENTS } from 'constants';
+import StepsHeader from './StepsHeader';
+import './styles.css';
+
+function Orders() {
+    return (
+        <div className="orders-container">
+            <StepsHeader />
+        </div>
+    )
+}
+
+export default Orders;
